ebacb7c3e8316de85f570429b72afe31e5c7357f
driftmeisterbj <ak.stra88@gmail.com>
2025-10-22 21:28:49 +0200

    Hello world

---
+++ app/Login.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import { styles } from "../styles";
+
+export default function Login() {
+  return (
+    <View style={styles.loginContainer}>
+      <Text>Hello world!</Text>
+    </View>
+  );
+}
---
+++ styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+const allStyles = StyleSheet.create({
+    loginContainer: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
+
+export const styles = StyleSheet.create({
+  ...allStyles,
+});
+  

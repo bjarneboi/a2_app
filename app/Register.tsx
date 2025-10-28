2d34d523a8354e459f7851e5472f08a846c47170
driftmeisterbj <ak.stra88@gmail.com>
2025-10-28 22:33:05 +0100

    Fulgt tutorial, skal finne ut hvordan transmisjon av data fungerer snart

--- app/Register.tsx
+++ app/Register.tsx
@@ -1,8 +1,53 @@
-import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  Alert,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import React from "react";
 import { styles } from "../styles";
 import InputField from "../components/InputField";
 import Button from "../components/Button";
+import auth from "@react-native-firebase/auth";
+import db from "@react-native-firebase/database";
+import { useNavigation } from "@react-navigation/native";
+import type { NativeStackNavigationProp } from "@react-navigation/native-stack";
+
+const [username, setUsername] = React.useState("");
+const [email, setEmail] = React.useState("");
+const [password, setPassword] = React.useState("");
+const [confirmPassword, setConfirmPassword] = React.useState("");
+
+const nav = useNavigation<NativeStackNavigationProp<any>>();
+
+const createProfile = async (response: any) => {
+  db()
+    .ref("/users/" + response.user.uid)
+    .set({
+      username,
+      email,
+    });
+};
+
+const registerUser = async () => {
+  if (email && password) {
+    try {
+      const response = await auth().createUserWithEmailAndPassword(
+        email,
+        password
+      );
+
+      if (response.user) {
+        await createProfile(response);
+        nav.replace("Main");
+      }
+    } catch (error) {
+      Alert.alert("Registration Error", (error as Error).message);
+    }
+  }
+};
 
 const Register = () => {
   return (
